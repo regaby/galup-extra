5573f5b6158c43e1109fecfb85f06ab0a366132e
Gabriela Rivero <regaby@gmail.com>
2018-09-10 20:05:20 -0300

    [UPD] hotel_reservation: condicion de check_reservation_rooms

--- hotel_reservation/models/hotel_reservation.py
+++ hotel_reservation/models/hotel_reservation.py
@@ -232,7 +232,7 @@ class HotelReservation(models.Model):
                   room_type_ids = room_type_obj.search([('cat_id', '=', room.product_id.categ_id.id)])
                   cap += room_type_ids.read(fields=['capacity'])[0]['capacity']
                   # cap += room.product_id.categ_id.capacity
-            if (self.adults + self.children) > cap:
+            if (self.adults + self.children) > cap and cap != 0:
                     raise ValidationError(_('Room Capacity \
                     Exceeded \n Please Select Rooms According to \
                     Members Accomodation.'))
