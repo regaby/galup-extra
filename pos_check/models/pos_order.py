452265d27296605ba8e972c7b595157d959af7a4
Gabriela Rivero <regaby@gmail.com>
2018-09-28 17:21:25 -0300

    [FIX] se revierte try

--- pos_check/models/pos_order.py
+++ pos_check/models/pos_order.py
@@ -43,49 +43,44 @@ class PosOrder(models.Model):
 
     def add_payment(self, cr, uid, order_id, data, context=None):
         statement_id = super(PosOrder, self).add_payment(cr, uid, order_id, data, context)
-        try: 
-            print 'order_id', order_id
-            print 'data', data
-            StatementLine = self.pool.get('account.bank.statement.line')
-            checkObj = self.pool.get('account.check')
-            check = {
-                'name' : data['check_number'],
-                'bank_id': data['check_bank_id'],
-                'journal_id': data['journal'],
-                'number': data['check_number'],
-                'amount': data['amount'],
-                'owner_name': data['check_owner'],
-                'owner_vat':  data['check_owner_vat'],
-                'issue_date': data['payment_date'][0:10],
-                'payment_date': data['check_pay_date'],
-                'type': 'third_check',
-            }
-            check_id = checkObj.create(cr, uid, check,context)
-            check_id = checkObj.browse(cr, uid, check_id)
-            self._add_operation(cr, uid, check_id, 'holding', False, data['check_pay_date'])
-            statement_lines = StatementLine.search(cr, uid, [
-                ('statement_id', '=', statement_id),
-                ('pos_statement_id', '=', order_id),
-                ('journal_id', '=', data['journal']),
-                ('amount', '=', data['amount'])
-            ])
-            for line in StatementLine.browse(cr, uid, statement_lines):
-                if line.journal_id.check_info_required and not line.check_bank_id:
-                    check_bank_id = data.get('check_bank_id')
-                    if isinstance(check_bank_id, (tuple, list)):
-                        check_bank_id = check_bank_id[0]
+        StatementLine = self.pool.get('account.bank.statement.line')
+        checkObj = self.pool.get('account.check')
+        check = {
+            'name' : data['check_number'],
+            'bank_id': data['check_bank_id'],
+            'journal_id': data['journal'],
+            'number': data['check_number'],
+            'amount': data['amount'],
+            'owner_name': data['check_owner'],
+            'owner_vat':  data['check_owner_vat'],
+            'issue_date': data['payment_date'][0:10],
+            'payment_date': data['check_pay_date'],
+            'type': 'third_check',
+        }
+        check_id = checkObj.create(cr, uid, check,context)
+        check_id = checkObj.browse(cr, uid, check_id)
+        self._add_operation(cr, uid, check_id, 'holding', False, data['check_pay_date'])
+        statement_lines = StatementLine.search(cr, uid, [
+            ('statement_id', '=', statement_id),
+            ('pos_statement_id', '=', order_id),
+            ('journal_id', '=', data['journal']),
+            ('amount', '=', data['amount'])
+        ])
+        for line in StatementLine.browse(cr, uid, statement_lines):
+            if line.journal_id.check_info_required and not line.check_bank_id:
+                check_bank_id = data.get('check_bank_id')
+                if isinstance(check_bank_id, (tuple, list)):
+                    check_bank_id = check_bank_id[0]
 
-                    check_vals = {
-                        'check_bank_id': check_bank_id,
-                        'check_bank_acc': data.get('check_bank_acc'),
-                        'check_number': data.get('check_number'),
-                        'check_owner': data.get('check_owner'),
-                        'check_owner_vat': data.get('check_owner_vat'),
-                        'check_pay_date': data.get('check_pay_date'),
-                    }
-                    line.write(check_vals)
-                    break
-        except Exception, e:
-            print e
+                check_vals = {
+                    'check_bank_id': check_bank_id,
+                    'check_bank_acc': data.get('check_bank_acc'),
+                    'check_number': data.get('check_number'),
+                    'check_owner': data.get('check_owner'),
+                    'check_owner_vat': data.get('check_owner_vat'),
+                    'check_pay_date': data.get('check_pay_date'),
+                }
+                line.write(check_vals)
+                break
 
         return statement_id
