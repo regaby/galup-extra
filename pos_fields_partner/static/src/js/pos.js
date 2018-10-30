69bf5f0c70e4e65c7a0ee5f85b15dcdec1f8148c
luciano <lucianogenessini@gmail.com>
2018-10-29 21:04:11 -0300

    [UPD] se logro hacer que traiga los atributos creados desde form, falta hacer que guarde

--- pos_fields_partner/static/src/js/pos.js
+++ pos_fields_partner/static/src/js/pos.js
@@ -1,8 +1,76 @@
 odoo.define('pos_fields_partner.PosModel', function (require) {
 "use strict";
 
-var models = require('point_of_sale.models');
-pos_model.load_fields("res.partner",['main_id_category_id']);
+var core = require('web.core');
+    var pos_model = require('point_of_sale.models');
+    var pos_chrome = require('point_of_sale.chrome')
+    var models = pos_model.PosModel.prototype.models;
+    var PosModelSuper = pos_model.PosModel;
+    var OrderSuper = pos_model.Order;
+    var OrderlineSuper = pos_model.Orderline; 
+    // var _super_posmodel = models.PosModel.prototype;
+
+    pos_model.load_fields("res.partner", ["main_id_category_id", "main_id_number"]);
+
+    for(var i=0; i<models.length; i++){
+        var model=models[i];
+        if(model.model === 'res.partner'){
+             model.fields.push('main_id_category_id');
+        }
+    }
+
+    models.push(
+        {
+            model: 'res.partner.id_category',
+            fields: ['id', 'name'],
+            loaded: function (self, category_id) {
+                for (var i in category_id){
+                    self.category_id.push(category_id[i]);
+                }
+            },
+        });
+
+    function get_category_id(category_id, name){
+        for(var i in category_id){
+            if(category_id[i].name == name){
+                return category_id[i].id;
+            }
+        }
+        return false;
+    }
+
+
+    pos_model.PosModel = pos_model.PosModel.extend({
+        initialize: function(session, attributes) {
+
+            PosModelSuper.prototype.initialize.call(this, session, attributes)
+            this.category_id = [];
+   
+        },
+    });
+
+
+
+    // new Model('res.partner').call('create_from_ui',[fields]).then(function(partner_id){
+    //         self.saved_client_details(partner_id);
+    //     },function(err,event){
+    //         event.preventDefault();
+    //         self.gui.show_popup('error',{
+    //             'title': _t('Error: Could not Save Changes'),
+    //             'body': _t('Your Internet connection is probably down.'),
+    //         });
+    //     });
+
+
+
+
+   
+
+   
+
+/*var models = require('point_of_sale.models');
+
+models.load_fields("res.partner",['main_id_category_id', 'website', 'main_id_number', 'function', 'fax']);
 
 models.load_models({
     model: 'res.partner.id_category',
@@ -14,17 +82,26 @@ models.load_models({
 }, {after: 'res.country'});
 
 
-
 var _super_posmodel = models.PosModel.prototype;
 models.PosModel = models.PosModel.extend({
 
     initialize: function (session, attributes) {
-            var partner_model = _.find(this.models, function(model){ return model.model === 'res.partner'; });
-            partner_model.fields.push('main_id_category_id');
+            var partner_model = _.find(this.models, function(models){ return model.model === 'res.partner'; });
+            partner_model.fields.push('website');
+            partner_model.fields.push('fax');
+            partner_model.fields.push('function');
+            partner_model.fields.push('main_id_number');
+        
             
-           this.main_id_category_id= main_id_category_id;
-           this.type_doc = type_doc;
+            
            return _super_posmodel.initialize.call(this, session, attributes);
         },
-},
+
+    start: function() {
+           this._super();
+           this.main_id_category_id= main_id_category_id;
+           this.type_doc = type_doc;
+
+    },   
+},*/
 });
