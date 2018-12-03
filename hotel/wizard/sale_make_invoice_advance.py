b7c2308afd1f13264f35802951f67c30d1ec4f4f
Gabriela Rivero <regaby@gmail.com>
2018-12-03 14:54:52 -0300

    [UPD] create_invoice: se agrega funcion (comentada) que elimina impuesto si está marcado earlycheckin/latecheckout

--- hotel/wizard/sale_make_invoice_advance.py
+++ hotel/wizard/sale_make_invoice_advance.py
@@ -56,8 +56,9 @@ class SaleAdvancePaymentInv(models.TransientModel):
     @api.multi
     def create_invoices(self):
         ctx = self.env.context.copy()
-        hotel = False
+        hotel = None
         invoice_obj = self.env['account.invoice']
+        tax_obj = self.env['account.tax']
         if self._context.get('active_model') == 'hotel.folio':
             hotel_fol = self.env['hotel.folio']
             hotel = hotel_fol.browse(self._context.get('active_ids',
@@ -65,18 +66,34 @@ class SaleAdvancePaymentInv(models.TransientModel):
             ctx.update({'active_ids': [hotel.order_id.id],
                         'active_id': hotel.order_id.id,
                         'invoice_origin': hotel.name})
-        res_id = invoice_obj.search([('origin','=',hotel.name),('state','!=','cancel')])
+        res_id = invoice_obj.search([('origin', '=', hotel.name), ('state', '!=', 'cancel')])
         delete = []
         for r in res_id:
-          delete.append(r.id)
+            delete.append(r.id)
         res = super(SaleAdvancePaymentInv,
                     self.with_context(ctx)).create_invoices()
-        res_id = invoice_obj.search([('origin','=',hotel.name),('state','!=','cancel'),('id','not in',delete)])
+        res_id = invoice_obj.search([('origin', '=', hotel.name), ('state', '!=', 'cancel'),
+        	                            ('id', 'not in', delete)])
+        ## si está marcado early checkin / checkout, quito el impuesto
+        # for line in res_id.invoice_line_ids:
+        #     tax_ids = line.invoice_line_tax_ids.ids
+        #     if hotel.early_checkin:
+        #         tax_id = tax_obj.search([('check_type', '=', 'early'),
+        #         	                        ('hour_from', '<=', hotel.early_checkin_hour),
+        #         	                        ('hour_to', '>=', hotel.early_checkin_hour)])
+        #         tax_ids.remove(tax_id.id)
+        #     if hotel.late_checkout:
+        #         tax_id = tax_obj.search([('check_type', '=', 'late'),
+        #         	                        ('hour_from', '<=', hotel.late_checkout_hour),
+        #         	                        ('hour_to', '>=', hotel.late_checkout_hour)])
+        #         tax_ids.remove(tax_id.id)
+        #     line.invoice_line_tax_ids = [(6, 0, tax_ids)]
+        ## fin
         if hotel and res_id:
             if res_id.partner_id.parent_id:
                 res_id.partner_id = res_id.partner_id.parent_id.id
             hotel.write({'hotel_invoice_id': res_id.id})
-        line_ids = self.env['account.invoice.line'].search([('invoice_id','=',res_id.id)])
+        line_ids = self.env['account.invoice.line'].search([('invoice_id', '=', res_id.id)])
         for line in line_ids:
-            line.name='NOCHE DE ALOJAMIENTO'
+            line.name = 'NOCHE DE ALOJAMIENTO'
         return res
